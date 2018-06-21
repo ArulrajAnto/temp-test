e2561bb50eefa8c7ef5be47bd181509b5c356249
ArulrajAnto <antopsnec@gmail.com>
2018-06-21 17:35:56 +0530

    test1

--- encrypted1.py
+++ encrypted1.py
@@ -1,5 +1,5 @@
 from boto3 import client
 
 conn = client('s3')  # again assumes boto.cfg setup, assume AWS S3
-for key in conn.list_objects(Bucket='lambda-vishal')['Contents']:
+for key in conn.list_objects(Bucket='antotest')['Contents']:
     print(key['Key'])
